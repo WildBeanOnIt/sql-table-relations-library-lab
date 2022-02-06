14ddda8b1504a8ad7a9a47fae1dadf01ef7baa9b
Julio Aguilera Jr <julioaguilera337@gmail.com>
2022-02-06 13:25:57 -0600

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,8 +16,16 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
-end
+  "SELECT series.title,
+  FROM series,
+  INNER JOIN books,
+  ON series.id = books.series_id,
+  INNER JOIN character_books,
+  ON books.id = character_books.book_id,
+  INNER JOIN characters,
+  ON character_books.character_id = characters.id,
+  WHERE characters.species = 'human' GROUP BY series.title ORDER BY(\*) DESC LIMIT 1;"
+  end
 
 def select_character_names_and_number_of_books_they_are_in
   "Write your SQL query here"
